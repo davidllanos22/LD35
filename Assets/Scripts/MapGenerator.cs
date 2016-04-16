0f6291f094171a90089cc2062507109d9b1eb954
David Llanos <davidllanos22@gmail.com>
2016-04-16 12:32:19 +0200

    First commit.

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int width, height;
+	
+    [Range(0,100)]
+    public int randomFillPercent;
+
+    public string seed;
+    public bool useRandomSeed;
+   
+    int [,] map;
+    public int borderWidth = 5;
+
+    void Start(){
+        GenerateMap();
+    }
+
+    void Update(){
+        if(Input.GetMouseButtonDown(0)){
+            GenerateMap();
+        }
+    }
+
+    void GenerateMap(){
+        map = new int[width, height];
+        RandomFillMap();
+
+        for(int i = 0; i< 5; i++){
+            SmoothMap();
+        }
+    }
+
+    void RandomFillMap(){
+        if(useRandomSeed){
+            seed = Time.time.ToString();
+        }
+
+        System.Random random = new System.Random(seed.GetHashCode());
+        for(int x = 0; x < width; x++){
+            for(int y = 0; y < height; y++){
+                if(x <= borderWidth || x >= width - borderWidth || y <= borderWidth || y >= height - borderWidth){
+                    map[x, y] = 1;
+                }else{
+                    map[x, y] = random.Next(0, 100) < randomFillPercent ? 1 : 0;
+                }
+            }   
+        }
+    }
+
+    void SmoothMap(){
+        for(int x = 0; x < width; x++){
+            for(int y = 0; y < height; y++){
+                int numTiles = GetNeighbourTiles(x, y);
+                if(numTiles > 4){
+                    map[x, y] = 1;
+                }else if(numTiles < 4){
+                    map[x, y] = 0;
+                }
+            }   
+        }
+    }
+
+    int GetNeighbourTiles(int tX, int tY){
+        int wallCount = 0;
+        for(int nX = tX - 1; nX <= tX + 1; nX++){
+            for(int nY = tY -1; nY <= tY + 1; nY++){
+                if(nX >= 0 && nX < width && nY >= 0 && nY < height){
+                    if(nX != tX || nY != tY){
+                        wallCount += map[nX, nY];
+                    }
+                }else{
+                    wallCount++;
+                }
+            }
+        }
+        return wallCount;
+    }
+
+    void OnDrawGizmos(){
+        if(map != null){
+            for(int x = 0; x < width; x++){
+                for(int y = 0; y < height; y++){
+                    Gizmos.color = map[x, y] == 1 ? Color.blue: Color.yellow;
+                    Vector3 pos = new Vector3(-width/2 + x + .5f, -height/2 + y + .5f, 0);
+                    Gizmos.DrawCube(pos, Vector3.one);
+                }   
+            }
+        }
+    }
+
+}
