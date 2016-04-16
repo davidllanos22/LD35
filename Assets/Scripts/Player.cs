4d67fcdde9b508dba2fae12efa86b775b99ac01b
David Llanos <davidllanos22@gmail.com>
2016-04-16 17:41:07 +0200

    Arreglado bug movimiento personaje y bug de obtener tile al clickar.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,32 @@ public class Player : MonoBehaviour {
     private Vector3 targetTile;
     private SpriteRenderer renderer;
 
+    private float lerpTime = 1f;
+    private float currentLerpTime = 0;
+
 	// Use this for initialization
 	void Start () {
         currentTile = new Vector3(-5, 0, -1);
         targetTile = currentTile;
         renderer = transform.GetComponent<SpriteRenderer>();
-        transform.position = currentTile;
+        transform.position = targetTile;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        
+
+        currentLerpTime += 0.01f;
+        if(currentLerpTime > lerpTime){
+            currentLerpTime = lerpTime;
+        }
+
+        float perc = currentLerpTime / lerpTime;
+
+        transform.position = Vector3.Lerp(transform.position, targetTile, perc);
+	}
+
+    void Update(){
         if(Input.GetMouseButtonDown(0)){
             GameObject tileClicked = GetTileClicked();
             tileClicked.GetComponent<SpriteRenderer>().color = Color.red;
@@ -25,13 +41,11 @@ public class Player : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.A)) SetPosition(-1, 0);
         if(Input.GetKeyDown(KeyCode.S)) SetPosition(0, 1);
         if(Input.GetKeyDown(KeyCode.D)) SetPosition(1, 0);
-
-
-        transform.position = Vector3.Lerp(transform.position, targetTile, Time.deltaTime * 10f);
-	}
+    }
 
     private void SetPosition(int offX, int offY){
-        targetTile += new Vector3(offX, -offY, 0); 
+        targetTile += new Vector3(offX, -offY, 0);
+        currentLerpTime = 0;
     }
 
     private GameObject GetTileClicked(){
